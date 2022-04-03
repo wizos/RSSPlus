b452b40998db9f73e81a17a0d07a975e67e03e8e
“Wizos” <wizos@qq.com>
2022-04-04 02:28:31 +0800

    test

--- api/find.js
+++ api/find.js
@@ -10,7 +10,7 @@ export default function handler(request, response) {
 	}
 
 	if(!url){
-		return response = new Response('Missing parameter: url.', {status: 500});
+		return new Response('Missing parameter: url.', {status: 500});
 	}else{
 		response.status(200).text("测试成功");
 	}
