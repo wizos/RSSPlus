88789bc9309094ba5daff9a58264b3cc03acdc2f
“Wizos” <wizos@qq.com>
2022-04-05 00:27:12 +0800

    test

--- api/find.js
+++ api/find.js
@@ -3,28 +3,20 @@ function looseJsonParse(obj) {
 }
 
 export default function handler(request, response) {
-	let url;
-	if(request.query){
-		url = request.query.url;
-	}
-	if(!url && request.body){
-		url = request.body.get("url");
-	}
-
+	const { searchParams } = new URL(request.url);
+    let url = searchParams.get('url');
+    
 	if(!url){
 		response.status(500).send("查询参数中缺少 URL 字段");
 	}else{
-		var path = require('path'), fs = require('fs');
-
-		//console.log( "路径2：" + path.resolve('.', 'Ruler.js') );
+		const path = require('path'), fs = require('fs');
 
 		const filePath = path.resolve('.', 'Ruler.js');
 		const emberjs = fs.readFileSync(filePath, 'utf8');
 		//eval(emberjs);
-		var Ruler = looseJsonParse(emberjs);
-		console.log( "路径3：" + emberjs );
+		console.log("请求：" + url);
+		const Ruler = looseJsonParse(emberjs);
 
-		
 		response.status(200).send(Ruler.find(url));
 	}
 }
