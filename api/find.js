e6c87ebc3d2f6bbeaf5248ffb0abeebd280147cb
“Wizos” <wizos@qq.com>
2022-04-04 20:15:35 +0800

    test

--- api/find.js
+++ api/find.js
@@ -1,15 +1,17 @@
 var path = require('path'), fs = require('fs');
 
 
-var root = path.join(path.dirname(fs.realpathSync(__filename)), '../'); 
-var emberjs = fs.readFileSync( root  + '/Ruler.js', 'utf8');
-eval(emberjs);
-
 console.log( "路径1：" + fs.realpathSync(__filename) );
 console.log( "路径2：" + path.dirname(fs.realpathSync(__filename)) );
 
 console.log( "路径3：" + path.join(path.dirname(fs.realpathSync(__filename)), '../') );
 console.log( "路径4：" + path.join(path.dirname(fs.realpathSync(__filename)), '..') );
+
+
+var root = path.join(path.dirname(fs.realpathSync(__filename)), '../'); 
+var emberjs = fs.readFileSync( root  + '/Ruler.js', 'utf8');
+eval(emberjs);
+
 
 export default function handler(request, response) {
 	let url;
