bd37c617ea67231a571693b4dc787c5d11c12d2a
“Wizos” <wizos@qq.com>
2022-04-04 21:13:27 +0800

    test

--- api/find.js
+++ api/find.js
@@ -1,19 +1,3 @@
-var path = require('path'), fs = require('fs');
-
-
-console.log( "路径1：" + fs.realpathSync(__filename) );
-console.log( "路径2：" + path.resolve('.', 'Ruler.js') );
-console.log( "路径3：" + path.join(path.dirname(fs.realpathSync(__filename)), '../') );
-
-
-const filePath = path.resolve('.', 'Ruler.js');
-const emberjs = fs.readFileSync(filePath, 'utf8');
-//var root = path.join(path.dirname(fs.realpathSync(__filename)), '../'); 
-//var emberjs = fs.readFileSync( root  + 'Ruler.js', 'utf8');
-
-eval(emberjs);
-
-
 export default function handler(request, response) {
 	let url;
 	if(request.query){
@@ -26,6 +10,17 @@ export default function handler(request, response) {
 	if(!url){
 		response.status(500).send("Missing parameter: url.");
 	}else{
+		var path = require('path'), fs = require('fs');
+
+		console.log( "路径1：" + fs.realpathSync(__filename) );
+		console.log( "路径2：" + path.resolve('.', 'Ruler.js') );
+		console.log( "路径3：" + path.join(path.dirname(fs.realpathSync(__filename)), '../') );
+
+		const filePath = path.resolve('.', 'Ruler.js');
+		const emberjs = fs.readFileSync(filePath, 'utf8');
+		eval(emberjs);
+
+		
 		response.status(200).send(Ruler.find(url));
 	}
 }
